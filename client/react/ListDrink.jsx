3042c9ebd413f078eeb944391d396d89b5150ce8
Alexey Levanov <ilewanof@gmail.com>
2016-01-14 21:00:10 +0300

    Подключен компонент Drink.jsx

--- client/react/ListDrink.jsx
+++ client/react/ListDrink.jsx
@@ -1,7 +1,9 @@
 'use strict';
 import React from 'react';
 import mui from 'material-ui';
-import Firebase from 'firebase'
+import Firebase from 'firebase';
+import Drink from './Drink.jsx';
+import _ from 'lodash';
 import ActionInfo from 'material-ui/lib/svg-icons/action/info';
 import SocialPeople from 'material-ui/lib/svg-icons/social/people';
 import Colors from 'material-ui/lib/styles/colors';
@@ -39,42 +41,23 @@ class ListDrink extends React.Component {
         this.loadDataFromFirebase();
     }
     render() {
-        console.log(this.state.listDrinks)
+        var drinkNodes = _(this.state.listDrinks)
+            .keys()
+            .map((k, i) => {
+                let drink = this.state.listDrinks[k];
+                console.log(drink)
+                return (
+                    <Drink drink={drink} key={i}/>
+                )
+            })
+            .value();
+        console.log(drinkNodes);
+
         return(
                 <Card>
-                    <List subheader="BEER" insetSubheader={true} >
-                        <ListItem
-                            leftAvatar={<Avatar icon={<SocialPeople />} />}
-                            rightIcon={<ActionInfo />}
-                            primaryText="Мохнатый шмель"
-                            secondaryText="Jan 9, 2016" />
-                        <ListItem
-                            leftAvatar={<Avatar icon={<SocialPeople />} />}
-                            rightIcon={<ActionInfo />}
-                            primaryText="Балтика №9"
-                            secondaryText="Jan 10, 2016" />
-                        <ListItem
-                            leftAvatar={<Avatar icon={<SocialPeople />} />}
-                            rightIcon={<ActionInfo />}
-                            primaryText="Клинское"
-                            secondaryText="Jan 7, 2016" />
-                    </List>
+                    <Drink />
                     <Divider inset={true} />
-                    <List subheader="COCTAIL" insetSubheader={true}>
-                        <ListItem
-                            leftAvatar={<Avatar icon={<SocialPeople />} backgroundColor={Colors.blue500} />}
-                            rightIcon={<ActionInfo />}
-                            primaryText="Vacation itinerary"
-                            secondaryText="Jan 20, 2014" />
-                        <ListItem
-                            leftAvatar={<Avatar icon={<SocialPeople />} backgroundColor={Colors.yellow600} />}
-                            rightIcon={<ActionInfo />}
-                            primaryText="Kitchen remodel"
-                            secondaryText="Jan 10, 2014" />
-                    </List>
                 </Card>
-
-
         )
     }
 }
